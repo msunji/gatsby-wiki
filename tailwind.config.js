0566a4c3247c055ca1a99e23106c98e4bb73757b
Marge <marge.consunji@gmail.com>
2022-01-25 15:40:08 +0800

    Set theme colours in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {},
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      slate: colors.slate,
+      cyan: colors.cyan,
+    },
+    extend: {
+      gridTemplateColumns: {
+        main: '300px 1fr',
+      },
+    },
+  },
   plugins: [],
 };

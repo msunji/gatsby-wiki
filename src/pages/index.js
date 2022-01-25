f6e47791c376a0dc7160cab295712b612b3048af
Marge <marge.consunji@gmail.com>
2022-01-25 16:01:55 +0800

    Wrap in Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
 import * as React from 'react';
+import Layout from '../components/Layout';
 
 const IndexPage = () => {
-  return <main className="pt-16">Test</main>;
+  return (
+    <Layout>
+      <article className="prose">
+        <h1>Test</h1>
+        <hr />
+      </article>
+    </Layout>
+  );
 };
 
 export default IndexPage;

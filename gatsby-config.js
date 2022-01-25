3a2c14faa022d2cf2608d9d8969723fee30503b1
Marge <marge.consunji@gmail.com>
2022-01-25 15:19:20 +0800

    Init new Gatsby project
    I've already installed Tailwind CSS and done basic set-up for it with this commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,27 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sharp", "gatsby-plugin-mdx", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-mdx',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
     },
-    __key: "pages"
-  }]
+  ],
 };

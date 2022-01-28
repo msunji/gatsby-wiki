2c1bd89cad106c8d3f50fd70922bc25dc8e87547
Marge <marge.consunji@gmail.com>
2022-01-28 10:59:59 +0800

    Remove Tailwind Typography code block styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,25 @@ module.exports = {
       cyan: colors.cyan,
       white: colors.white,
     },
+
     extend: {
       gridTemplateColumns: {
         main: '300px 1fr',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            pre: false,
+            code: {
+              '&::before': {
+                content: 'none !important',
+              },
+              '&::after': {
+                content: 'none !important',
+              },
+            },
+          },
+        },
       },
     },
   },

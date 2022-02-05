0bd8619aecac6933e8c62a1cc0409ae37ff9ead7
Marge <marge.consunji@gmail.com>
2022-02-05 15:33:02 +0800

    Add placeholder text - needs work

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,11 @@ const IndexPage = () => {
   return (
     <Layout>
       <article className="prose">
-        <h1>Test</h1>
-        <hr />
+        <h1>About</h1>
+        <p>
+          Hi! You've found Marge's personal wiki. This project's been inspired
+          by other folks's personal wikis and digital gardens.{' '}
+        </p>
       </article>
     </Layout>
   );

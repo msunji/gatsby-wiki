a2d87991b416946bc213dc97ef5b1e08a4d800e2
Marge <marge.consunji@gmail.com>
2022-02-07 15:38:13 +0800

    Add wrapPageElement function
    Do this so that the Layout component persists and doesn't unmount on route change.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
+import React from 'react';
 import './src/styles/global.css';
+import Layout from './src/components/Layout';
+
+// This is for wrapping every page in the wiki with the Layout component
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>;
+};

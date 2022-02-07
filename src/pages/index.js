4cc48292b8fb4601d3c72ae9509ce4089e705017
Marge <marge.consunji@gmail.com>
2022-02-07 16:41:43 +0800

    Update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,26 @@ const IndexPage = () => {
     <article className="prose">
       <h1>About</h1>
       <p>
-        Hi! You've found Marge's personal wiki. This project's been inspired by
-        other folks's personal wikis and digital gardens.{' '}
+        Hey there! This is my personal wiki where you'll find notes I've made
+        for baking, programming, and everything in between. These notes are
+        grouped by category and are updated, well, when they need to be updated,
+        I suppose.
       </p>
+      <p>
+        I've mostly made these notes for my own future reference, but if you
+        find any of these notes helpful, then that'd make me pretty darn happy
+        😊
+      </p>
+
+      <h2>Where to find me</h2>
+      <ul>
+        <li>
+          <a href="https://github.com/msunji/">Github</a>
+        </li>
+        <li>
+          <a href="https://www.mconsunji.com/">Portfolio</a>
+        </li>
+      </ul>
     </article>
   );
 };

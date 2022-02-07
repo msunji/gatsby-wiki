91a5e8267d7a5cc403160f7cb6df2aa37585a6a5
Marge <marge.consunji@gmail.com>
2022-02-07 15:38:02 +0800

    Remove Layout component import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
 import * as React from 'react';
-import Layout from '../components/Layout';
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <article className="prose">
-        <h1>About</h1>
-        <p>
-          Hi! You've found Marge's personal wiki. This project's been inspired
-          by other folks's personal wikis and digital gardens.{' '}
-        </p>
-      </article>
-    </Layout>
+    <article className="prose">
+      <h1>About</h1>
+      <p>
+        Hi! You've found Marge's personal wiki. This project's been inspired by
+        other folks's personal wikis and digital gardens.{' '}
+      </p>
+    </article>
   );
 };
-
 export default IndexPage;

e02f7cce8dfbaa39b67303fe0016df32b907c96e
Marge <marge.consunji@gmail.com>
2022-02-07 15:37:46 +0800

    Remove Layout component import

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import Layout from '../components/Layout';
 
 const NotFoundPage = () => {
   return (
-    <Layout>
-      <article className="prose">
-        <h1>Whoops.</h1>
-        <p>It looks like you've reached a page that doesn't exist.</p>
-      </article>
-    </Layout>
+    <article className="prose">
+      <h1>Whoops.</h1>
+      <p>It looks like you've reached a page that doesn't exist.</p>
+    </article>
   );
 };
 

6eb63658b8b560cfa598051bbc70f2f428639993
Marge <marge.consunji@gmail.com>
2022-02-07 16:44:24 +0800

    Import React

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // This is for wrapping every page in the wiki with the Layout component
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;

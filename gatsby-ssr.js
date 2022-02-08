89fe4cc3038a16719becdd5132954fc7ee3c7589
Marge <marge.consunji@gmail.com>
2022-02-08 14:29:54 +0800

    Correct missing import

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Layout from './src/components/Layout';
 
 // This is for wrapping every page in the wiki with the Layout component
 export const wrapPageElement = ({ element, props }) => {

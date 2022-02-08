4364a28b23080f43fe1923dda31e5c3350199fb2
Marge <marge.consunji@gmail.com>
2022-02-08 13:41:21 +0800

    Finetune SEO component, remove bits I don't need

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,61 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { useLocation } from '@react/router';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
-const SEO = ({ title, description, image, article }) => {
-  // const { pathname } = useLocation();
+const Seo = ({ title, description, image, article }) => {
   const { site } = useStaticQuery(query);
 
-  const {
-    defaultTitle,
-    titleTemplate,
-    defaultDescription,
-    siteUrl,
-    icon,
-    // defaultImage,
-  } = site.siteMetadata;
+  const { defaultTitle, titleTemplate, defaultDescription } = site.siteMetadata;
 
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    icon: `${icon}`,
-    // image: `${siteUrl}${image || defaultImage}`,
-    // url: `${siteUrl}${pathname}`,
   };
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <meta name="description" content={seo.description} />
-      <meta name="icon" href={seo.icon} />
-      <meta name="image" content={seo.image} />
-      {/* {seo.url && <meta property="og:url" content={seo.url} />} */}
-      {(article ? true : null) && <meta property="og:type" content="article" />}
+      <link rel="icon" href="https://emojicdn.elk.sh/🍮" />
       {seo.title && <meta property="og:title" content={seo.title} />}
       {seo.description && (
         <meta property="og:description" content={seo.description} />
       )}
-      {/* {seo.image && <meta property="og:image" content={seo.image} />} */}
     </Helmet>
   );
 };
 
-SEO.propTypes = {
+Seo.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  image: PropTypes.string,
-  article: PropTypes.bool,
 };
 
-SEO.defaultProps = {
+Seo.defaultProps = {
   lang: `en`,
   title: null,
   description: null,
-  image: null,
-  article: false,
 };
 
-export default SEO;
+export default Seo;
 
 const query = graphql`
   query SEO {

1f84fe26834132994c822a635e1128c172dd9022
Marge <marge.consunji@gmail.com>
2022-04-18 17:29:40 +0800

    Wrap root with provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './src/styles/global.css';
 import Layout from './src/components/Layout';
+import ThemeProvider from './src/utils/ThemeProvider';
+
+export const wrapRootElement = ThemeProvider;
 
 // This is for wrapping every page in the wiki with the Layout component
 export const wrapPageElement = ({ element, props }) => {
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+import ThemeProvider from './src/utils/ThemeProvider';
+
+export const wrapRootElement = ThemeProvider;
 
 // This is for wrapping every page in the wiki with the Layout component
 export const wrapPageElement = ({ element, props }) => {

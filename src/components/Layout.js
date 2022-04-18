6c8298407126c2000155ca88e9ed4deb2fa11a38
Marge <marge.consunji@gmail.com>
2022-04-18 14:05:09 +0800

    Change site layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import Nav from './Nav';
 import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <main className="min-h-screen md:gap-x-10 md:flex">
-      <Sidebar />
-      <div className="pt-28 pb-24 md:pt-20 container mx-auto w-11/12 h-full">
-        {children}
-      </div>
+    <main className="min-h-screen container mx-auto mt-10 pb-24 px-5">
+      <Nav />
+      {/* <Sidebar /> */}
+      <section className="mt-8">{children}</section>
     </main>
   );
 };

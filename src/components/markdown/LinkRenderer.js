cc68e0921d6404b9d3c1f778a53f6f571f86a06b
Marge <marge.consunji@gmail.com>
2022-04-19 15:01:06 +0800

    Make custom Link component
    This parses backlinks and external links differently.

---
+++ src/components/markdown/LinkRenderer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const LinkRenderer = ({ href, children }) => {
+  if (href.includes('.md')) {
+    const [category, route] = href.split('/');
+    let parsedRoute = route.replace(/(.md)/g, '').replace(/(%20)/g, '-');
+    return <Link to={`/${category}/${parsedRoute}`}>{children}</Link>;
+  }
+  return <a>{children}</a>;
+};
+
+export default LinkRenderer;

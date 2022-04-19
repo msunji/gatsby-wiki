e355b61986f4d5f07284e0098198f8e372bca11f
Marge <marge.consunji@gmail.com>
2022-04-19 14:52:32 +0800

    Add dark mode toggle

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Moon from './Moon';
 import { Link } from 'gatsby';
 
-const Nav = () => {
+const Nav = ({ darkMode }) => {
   return (
     <nav className="max-w-[70ch] mx-auto flex items-center justify-between py-8">
       <h1 className="font-bold text-xl">
         <Link to="/">From the Desk Of</Link>
       </h1>
-      <div>Dark Mode</div>
+      <button onClick={darkMode}>
+        <Moon />
+      </button>
     </nav>
   );
 };

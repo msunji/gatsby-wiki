2e29ad56206f1cdc944ea50c1d366f56f848f62e
Marge <marge.consunji@gmail.com>
2022-04-19 14:50:17 +0800

    Generate .md instead of .mdx

--- plopfile.js
+++ plopfile.js
@@ -1,30 +1,32 @@
 module.exports = function (plop) {
   const dateNow = new Date(Date.now()).toISOString();
-  const datePosted = dateNow.split("T")[0];
+  const datePosted = dateNow.split('T')[0];
   plop.setGenerator('note', {
     description: 'Generates a new Markdown note',
     prompts: [
       {
-        type: "input",
-        name: "title",
-        message: "Note title:"
+        type: 'input',
+        name: 'title',
+        message: 'Note title:',
       },
       {
         type: 'input',
-        name: "category",
-        message: "Note category:"
+        name: 'category',
+        message: 'Note category:',
       },
       {
         type: 'input',
         name: 'datePosted',
         message: 'Date posted:',
-        default: `${datePosted}`
-      }
+        default: `${datePosted}`,
+      },
     ],
-    actions: [{
-      type: 'add',
-      path:'src/notes/{{lowerCase category}}/{{kebabCase title}}.mdx',
-      templateFile: 'src/templates/note-template.hbs',
-    }]
+    actions: [
+      {
+        type: 'add',
+        path: 'src/_notes/{{lowerCase category}}/{{kebabCase title}}.md',
+        templateFile: 'src/templates/note-template.hbs',
+      },
+    ],
   });
-}
+};

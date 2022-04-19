c04019fc949a941273986f4cdeb87b00ea0cae90
Marge <marge.consunji@gmail.com>
2022-04-19 14:59:36 +0800

    Clean up, add backlinks
    - Moved blockquote, code blocks, and links to their own components
    - LinkRenderer should now make some differentiation between backlinks (links to other existing notes) and external links

--- src/templates/note-template.js
+++ src/templates/note-template.js
@@ -1,65 +1,42 @@
 import React from 'react';
 import SeoComponent from '../components/SeoComponent';
+import Quote from '../components/markdown/Quote';
+import CodeBlock from '../components/markdown/CodeBlock';
+import LinkRenderer from '../components/markdown/LinkRenderer';
 import { graphql } from 'gatsby';
-import Highlight, { defaultProps } from 'prism-react-renderer';
-import shadesOfPurple from 'prism-react-renderer/themes/shadesOfPurple';
+
 import { MDXProvider } from '@mdx-js/react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 
-const CodeBlock = ({ children }) => {
-  const language = /language-(\w+)/.exec(children.props.className || '');
-  return (
-    <Highlight
-      {...defaultProps}
-      code={children.props.children}
-      language={language[1]}
-      theme={shadesOfPurple}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre style={style}>
-          <span className="relative -top-3 uppercase bg-red-500 text-white font-semibold tracking-widest px-4 py-1">
-            {language[1]}
-          </span>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
-  );
-};
-
 const components = {
   pre: CodeBlock,
+  blockquote: Quote,
+  a: LinkRenderer,
 };
 
 const NoteTemplate = ({ data }) => {
+  const { mdx } = data;
+  console.log(mdx.body);
   return (
     <>
-      <SeoComponent title={data.mdx.frontmatter.title} />
-      <div className="container md px-4">
-        <article className="prose prose-slate prose-h1:mb-1 prose-p:leading-relaxed prose-li:leading-normal prose-img:rounded-md prose-a:text-red-500 prose-blockquote:bg-slate-50 prose-blockquote:py-1 prose-blockquote:rounded-md prose-blockquote:border prose-blockquote:border-slate-200 prose-hr:my-8">
-          <div>
-            <h1>{data.mdx.frontmatter.title}</h1>
-            <p className="mb-0 text-sm">
-              <span className="uppercase">Posted:</span>{' '}
-              {data.mdx.frontmatter.datePosted}
-            </p>
-            <p className="mt-0 text-sm">
-              <span className="uppercase">Updated:</span>{' '}
-              {data.mdx.frontmatter.dateUpdated}
-            </p>
-          </div>
-          <hr />
-          <MDXProvider components={components}>
-            <MDXRenderer>{data.mdx.body}</MDXRenderer>
-          </MDXProvider>
-        </article>
-      </div>
+      <SeoComponent title={mdx.frontmatter.title} />
+      <>
+        <div>
+          <h1>{mdx.frontmatter.title}</h1>
+          <p className="mb-0 text-sm">
+            <span className="uppercase">Posted:</span>{' '}
+            {mdx.frontmatter.datePosted}
+          </p>
+          <p className="mt-0 text-sm">
+            <span className="uppercase">Updated:</span>{' '}
+            {mdx.frontmatter.dateUpdated}
+          </p>
+        </div>
+
+        <MDXProvider components={components}>
+          <MDXRenderer>{mdx.body}</MDXRenderer>
+        </MDXProvider>
+      </>
     </>
   );
 };

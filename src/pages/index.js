b9f0cbd7a61e8379d1d8ddfd69cebb9f33c0d792
Marge <marge.consunji@gmail.com>
2022-05-09 15:44:38 +0800

    Change copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,14 +15,13 @@ const IndexPage = ({
         <p>
           Hiya! This is my digital garden where you'll find short- and long-form
           notes about programming, baking, learning languages, and everything in
-          between. If you're new to the idea of a digital garden, you can read{' '}
+          between. If you're new to the idea of a digital garden, you can read
           <Link to="/learning/digital-gardens">this note about it</Link>.
         </p>
         <p>
-          But otherwise, tl;dr, it's kind of like a personal knowledge base - a
-          place where you can write about and explore different ideas. It's a
-          place for me to write about things and refine them over time. But also
-          a place for me to keep notes for future me to look back at.
+          tl;dr: this is a personal knowledge base where I write about and
+          explore different ideas and topics I'm interested in. Notes are added
+          and refined over time.
         </p>
         <h2>Notes I've written</h2>
         <ul>
